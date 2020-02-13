2b5c293a0601e76818443bb1b077340e229a4209
Kyle Talmage <ktalmage85@gmail.com>
2020-02-13 15:03:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,17 @@
-def reverse_each_word(string)
+def reverse_each_word_each(string)
   array = string.split(" ")
   new_array = []
   array.each do |string|
     new_array << string.reverse
   end
   new_array. join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array_two = []
+  array.collect do |string|
+    array_two << string.reverse
+  end
+  array_two.join(" ")
 end
